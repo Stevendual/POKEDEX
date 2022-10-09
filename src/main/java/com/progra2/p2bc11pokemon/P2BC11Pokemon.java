4fa5a5227d688f13b6dc6a2d98b1daa18889a640
Bryan Steven Duque Alas <110703261+Stevendual@users.noreply.github.com>
2022-10-08 19:13:24 -0600

    Add files via upload

---
+++ src/main/java/com/progra2/p2bc11pokemon/P2BC11Pokemon.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.progra2.p2bc11pokemon;
+
+/**
+ *
+ * @author dual4
+ */
+public class P2BC11Pokemon {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
